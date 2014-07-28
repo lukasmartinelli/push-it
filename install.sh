5c8808e92a0a1981deba1aff517f4ddce38f18a6
Lukas Martinelli <me@lukasmartinelli.ch>
2014-07-28 14:14:22 +0200

    Made pre push executable

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 if [ -z "$1" ]; then
     echo "Specify repository to install pre-push hook"
+    exit 0
 fi
 
 TARGET_REPO=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SOUND="$DIR/push-it.wav"
+PRE_PUSH=$TARGET_REPO/.git/hooks/pre-push
 
 if which aplay >/dev/null; then
-    cat > $TARGET_REPO/.git/hooks/pre-push <<-EOL
-        #!/bin/sh
-        aplay $SOUND
+    cat > $PRE_PUSH <<EOL
+#!/bin/sh
+aplay $SOUND
 EOL
+    chmod +x $PRE_PUSH
     echo "Created pre-push hook in $TARGET_REPO"
     exit 1
 else

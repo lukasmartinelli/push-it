4d03dd52cb2fff9256de2f548885e10b2f770f8b
Honza Gál <honza.gal@gmail.com>
2017-11-02 11:02:48 +0100

    Fixed install.sh for macOS

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ PRE_PUSH=$TARGET_REPO/.git/hooks/pre-push
 
 if which aplay >/dev/null; then
     PLAYER=aplay
-elif which aflay >/dev/null; then
+elif which afplay >/dev/null; then
     PLAYER=afplay
 else
     echo "Could not find alsa-utils (Linux) or afplay (OSX)"
@@ -23,7 +23,7 @@ SOUND[0]="$DIR/push-it.wav"
 SOUND[1]="$DIR/selfie.wav"
 SOUND[2]="$DIR/that-was-easy.wav"
 RANDOM_SOUND=\$[\$[ RANDOM % 3]]
-aplay \${SOUND[\$RANDOM_SOUND]}
+$PLAYER \${SOUND[\$RANDOM_SOUND]}
 EOL
 chmod +x $PRE_PUSH
 echo "Created pre-push hook in $TARGET_REPO"
